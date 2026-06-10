0a28f5a7911acb7c8089abf8b8480d2fc5c64b3e
Yennifer Cell <yennifer.cell@example.com>
2026-06-10 20:10:28 +0300

    changes to be committed: e Please enter the commit message for your changes. Lines starting

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,41 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+    artists_count = artist_count
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Update shared class-level song statistics whenever a song is created.
+        self.add_song_to_count()
+        self.add_to_genres(self.genre)
+        self.add_to_artists(self.artist)
+        self.add_to_genre_count(self.genre)
+        self.add_to_artists_count(self.artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre=None):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist=None):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre=None):
+        cls.genre_count[genre] = cls.genre_count.get(genre, 0) + 1
+
+    @classmethod
+    def add_to_artists_count(cls, artist=None):
+        cls.artist_count[artist] = cls.artist_count.get(artist, 0) + 1
